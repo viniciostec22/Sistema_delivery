35ccea2813f29f4268f6993b0c1f4e2704b551a0
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2023-03-10 15:04:09 -0300

    filtragem-categoria  e detalhes do produto

--- produto/urls.py
+++ produto/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("categoria/<int:id>", views.categorias, name='categoria'),
+    path("produto/<int:id>", views.produto, name='produto'),
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 def home(request):
-  return render(request, 'home.html')
+    if not request.session.get('carrinho'):
+        request.session['carrinho'] = []
+        request.session.save()
+    produtos = Produto.objects.all()
+    categorias = Categoria.objects.all()
+    return render(request, 'home.html', {'produtos': produtos,
+                                        'carrinho': len(request.session['carrinho']),
+                                        'categorias': categorias,
+                                        })
+    
+def categorias(request, id):
+    if not request.session.get('carrinho'):
+        request.session['carrinho'] = []
+        request.session.save()
+    produtos = Produto.objects.filter(categoria_id = id)
+    categorias = Categoria.objects.all()
+
+    return render(request, 'home.html', {'produtos': produtos,
+                                        'carrinho': len(request.session['carrinho']),
+                                        'categorias': categorias,})
+    
+
+def produto(request, id):
+    if not request.session.get('carrinho'):
+        request.session['carrinho'] = []
+        request.session.save()
+    erro = request.GET.get('erro')
+    produto = Produto.objects.filter(id=id)[0]
+    categorias = Categoria.objects.all()
+    return render(request, 'produto.html', {'produto': produto, 
+                                            'carrinho': len(request.session['carrinho']),
+                                            'categorias': categorias,
+                                            'erro': erro})
