64e34f682f3e41d04539fc7b6622f23602263efc
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2023-03-22 07:59:00 -0300

    v2

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('produto.urls'))
+    path('', include('produto.urls')),
+    path('pedido/', include('pedido.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
